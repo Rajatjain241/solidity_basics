db6dcf15600f8eafb29bdbf82d03d09e2d337ef9
Your Name <you@example.com>
2025-02-28 17:57:14 +0530

    Initial commit

---
+++ src/16_Mapping.sol
@@ -0,0 +1,122 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.25;
+
+// The keyType can be any built-in value type, bytes, string, or any contract.
+// valueType can be any type including another mapping or an array.
+contract Mapping {
+    // Mapping from address to uint
+    mapping(address => uint256) public myMap;
+
+    function get(address _addr) public view returns (uint256) {
+        // Mapping always returns a value.
+        // If the value was never set, it will return the default value.
+        return myMap[_addr];
+    }
+
+    function set(address _addr, uint256 _i) public {
+        // Update the value at this address
+        myMap[_addr] = _i;
+    }
+
+    function remove(address _addr) public {
+        // Reset the value to the default value.
+        delete myMap[_addr];
+    }
+}
+
+contract NestedMapping {
+    // Nested mapping (mapping from address to another mapping)
+    mapping(address => mapping(uint256 => bool)) public nested;
+
+    function get(address _addr1, uint256 _i) public view returns (bool) {
+        // You can get values from a nested mapping
+        // even when it is not initialized
+        return nested[_addr1][_i];
+    }
+
+    function set(address _addr1, uint256 _i, bool _boo) public {
+        nested[_addr1][_i] = _boo;
+    }
+
+    function remove(address _addr1, uint256 _i) public {
+        delete nested[_addr1][_i];
+    }
+}
+
+library IterableMapping {
+    // Iterable mapping from address to uint;
+    struct Map {
+        address[] keys;
+        mapping(address => uint256) values;
+        mapping(address => uint256) indexOf;
+        mapping(address => bool) inserted;
+    }
+
+    function get(Map storage map, address key) public view returns (uint256) {
+        return map.values[key];
+    }
+
+    function getKeyAtIndex(Map storage map, uint256 index) public view returns (address) {
+        return map.keys[index];
+    }
+
+    function size(Map storage map) public view returns (uint256) {
+        return map.keys.length;
+    }
+
+    function set(Map storage map, address key, uint256 val) public {
+        if (map.inserted[key]) {
+            map.values[key] = val;
+        } else {
+            map.inserted[key] = true;
+            map.values[key] = val;
+            map.indexOf[key] = map.keys.length;
+            map.keys.push(key);
+        }
+    }
+
+    function remove(Map storage map, address key) public {
+        if (!map.inserted[key]) {
+            return;
+        }
+
+        delete map.inserted[key];
+        delete map.values[key];
+
+        uint256 index = map.indexOf[key];
+        address lastKey = map.keys[map.keys.length - 1];
+
+        map.indexOf[lastKey] = index;
+        delete map.indexOf[key];
+
+        map.keys[index] = lastKey;
+        map.keys.pop();
+    }
+}
+
+contract TestIterableMap {
+    using IterableMapping for IterableMapping.Map;
+
+    IterableMapping.Map private map;
+
+    function testIterableMap() public {
+        map.set(address(0), 0);
+        map.set(address(1), 100);
+        map.set(address(2), 200); // insert
+        map.set(address(2), 200); // update
+        map.set(address(3), 300);
+
+        for (uint256 i = 0; i < map.size(); i++) {
+            address key = map.getKeyAtIndex(i);
+            assert(map.get(key) == i * 100);
+        }
+
+        map.remove(address(1));
+
+        // keys = [address(0), address(3), address(2)]
+        assert(map.size() == 3);
+        assert(map.getKeyAtIndex(0) == address(0));
+        assert(map.getKeyAtIndex(1) == address(3));
+        assert(map.getKeyAtIndex(2) == address(2));
+    }
+}
